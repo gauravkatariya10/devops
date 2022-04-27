18521aa6d219a37647d68e33132e3e9a91313d66
gaurav katariya <gauravkatariya43@gmail.com>
2022-04-27 10:43:39 +0530

    updated

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 class Main{
 	public static void main(String[] args) {
-		System.out.println("enter 2 numbers");
+		
 		int a = 10;
  		int b = 20;
 		int sum  = a + b;
- 
-		System.out.println("enter 2 numbers");
+		System.out.println("a : " + a);
+		System.out.println("b : "+b);
+		System.out.println("enter 2 numbers::"+sum);
 }
 }
